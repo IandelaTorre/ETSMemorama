797cfdd986dce3b6f3e69cef9fbe3b04a1d988b3
Ian Perez <ian.perez@wundertec.com>
2025-07-14 17:52:23 -0600

    delete old files

--- app/src/main/java/com/example/memorama/GameEndHandler.kt
+++
@@ -1,6 +0,0 @@
-package com.example.memorama
-
-interface GameEndHandler {
-    fun onGameEndEarly()
-    fun onRevealSolution()
-}
--- app/src/main/java/com/example/memorama/Item.kt
+++
@@ -1,8 +0,0 @@
-package com.example.memorama
-
-data class Item(
-    val imageResId: Int,
-    val identifier: String,
-    var isFlipped: Boolean = false,
-    var isMatched: Boolean = false
-)
